b83346997f04b25c125d7dffbc79b261bad5af5b
minjeongnew <20171623sw@kookmin.ac.kr>
2021-06-29 14:38:43 +0900

    level1-체육복

---
+++ programmers/level1/gym_uniform.py
@@ -0,0 +1,15 @@
+def solution(n, lost, reserve):
+    reserve2 = [i for i in reserve if i not in lost]
+    lost2 = [j for j in lost if j not in reserve]
+
+    answer = n - len(lost2)
+    # 앞에서 빌려준 학생
+    b = []
+    for i in lost2:
+        for j in reserve2:
+            if j not in b and i in [j-1, j+1]:
+                answer +=1
+                b.append(i)
+                break
+
+    return answer
