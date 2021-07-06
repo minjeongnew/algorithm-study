7459e581e2eec0c7ff58da5fa51c07a824943316
minjeongnew <20171623sw@kookmin.ac.kr>
2021-07-06 15:49:36 +0900

    정렬 - 선택 정렬 - 불안정 정렬 - 제자리 정렬(in-place-sorting)

---
+++ data-structure/selection_sort.py
@@ -0,0 +1,29 @@
+# 선택 정렬
+# 주어진 데이터 내에서 가장 작은 데이터 찾기
+
+
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_ = arr[i]
+        loc = i
+        for j in range(i+1, len(arr)):
+            if min_ > arr[j]:
+                min_ = arr[j]
+                loc = j
+        arr[i], arr[loc] = arr[loc], arr[i]
+
+
+arr = [8, 6, 5, 3, 1, 2, 7, 4]
+
+selection_sort(arr)
+print(arr)
+
+
+# 착각한거
+# def selection_sort(arr):
+#     for i in range(len(arr)):
+#         for j in range(i+1, len(arr)):
+#             if arr[i] > arr[j]:
+#                 arr[i], arr[j] = arr[j], arr[i]
+# 위 코드는 주어진 데이터에서 최솟값을 찾는게 아니라
+# 가장 가까운 최솟값을 찾는 것이므로 선택정렬이 아님
