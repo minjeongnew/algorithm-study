5cd53ae031897349fb8058fb066825b9cd6950c0
minjeongnew <20171623sw@kookmin.ac.kr>
2021-08-02 12:10:05 +0900

    level1-숫자 문자열과 영단어

---
+++ programmers/level1/숫자_문자열과_영단어.py
@@ -0,0 +1,32 @@
+def solution(s):
+    answer = ''
+    n_list = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    n_dic = dict()
+    for i in range(10):
+        n_dic[n_list[i]] = str(i)
+    word = ''
+    for i in range(len(s)):
+        if s[i].isdecimal():
+            answer += s[i]
+        else:
+            word += s[i]
+            if word in n_dic:
+                answer += n_dic[word]
+                word = ''
+    return int(answer)
+
+
+def solution2(s):
+    n_list = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    n_dic = dict()
+    for i in range(10):
+        n_dic[n_list[i]] = str(i)
+    answer = s
+    for key, value in n_dic.items():
+        answer = answer.replace(key, value)
+    return answer
+
+
+if __name__ == '__main__':
+    s = "2three45sixseven"
+    print(solution2(s))
