74477f405f6e10580774b291895ebc5e2aff9847
minjeongnew <20171623sw@kookmin.ac.kr>
2021-09-10 15:07:25 +0900

    boj-gold4 dfs/1987(알파벳) 수정 bfs -> dfs

--- boj/gold/boj-gold4/1987-알파벳.py
+++ boj/gold/boj-gold4/1987-알파벳.py
@@ -1,20 +1,50 @@
 import sys
-from collections import deque
 
 
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 r, c = map(int, sys.stdin.readline().split())
-graph = [list(sys.stdin.readline().rstrip()) for _ in range(r)]
-q = deque()
-q.append([0, 0, graph[0][0]])
-answer = 0
-while q:
-    x, y, a = q.popleft()
+graph = [list(map(lambda x:ord(x)-65, sys.stdin.readline().rstrip())) for _ in range(r)]
+v = [0] * 26
+
+
+def dfs(x, y, cnt):
+    global answer
+    answer = answer if answer > cnt else cnt
     for k in range(4):
         nx = x + dx[k]
         ny = y + dy[k]
-        if 0 <= nx < r and 0 <= ny < c and graph[nx][ny] not in a:
-            q.append([nx, ny, a+graph[nx][ny]])
-            answer = max(answer, len(a)+1)
+        if 0 <= nx < r and 0 <= ny < c and v[graph[nx][ny]] == 0:
+            v[graph[nx][ny]] = 1
+            dfs(nx, ny, cnt+1)
+            v[graph[nx][ny]] = 0
+
+answer = 0
+v[graph[0][0]] = 1
+dfs(0, 0, 1)
 print(answer)
+
+
+
+### bfs -> 시간초과..
+
+# import sys
+# from collections import deque
+#
+#
+# dx = [-1, 0, 1, 0]
+# dy = [0, 1, 0, -1]
+# r, c = map(int, sys.stdin.readline().split())
+# graph = [list(sys.stdin.readline().rstrip()) for _ in range(r)]
+# q = deque()
+# q.append([0, 0, graph[0][0]])
+# answer = 0
+# while q:
+#     x, y, a = q.popleft()
+#     for k in range(4):
+#         nx = x + dx[k]
+#         ny = y + dy[k]
+#         if 0 <= nx < r and 0 <= ny < c and graph[nx][ny] not in a:
+#             q.append([nx, ny, a+graph[nx][ny]])
+#             answer = max(answer, len(a)+1)
+# print(answer)
