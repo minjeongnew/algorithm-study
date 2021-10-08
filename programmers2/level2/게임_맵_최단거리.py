fae8d3588d5d9f1a12340ba7e1e886dd8ba6e3bb
minjeongnew <20171623sw@kookmin.ac.kr>
2021-10-09 00:29:38 +0900

    level2 게임 맵 최단거리 + dfs 추가 그러나 프로그래머스에선 시간초과 발생

--- programmers2/level2/게임_맵_최단거리.py
+++ programmers2/level2/게임_맵_최단거리.py
@@ -18,3 +18,26 @@ def solution(maps):
                 q.append((nx, ny))
                 v[nx][ny] = v[x][y] + 1
     return v[-1][-1]
+
+
+import sys
+sys.setrecursionlimit(10**6)
+
+
+def solution_dfs(maps):
+    n, m = len(maps), len(maps[0])
+    v = [[10001]*m for _ in range(n)]
+    dx = [-1, 0, 1, 0]
+    dy = [0, 1, 0, -1]
+    def dfs(x, y, depth):
+        v[x][y] = depth
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+            if 0 <= nx < n and 0 <= ny < m and maps[nx][ny] == 1 and v[nx][ny] > depth+1:
+                dfs(nx, ny, depth+1)
+    dfs(0, 0, 1)
+    if v[-1][-1] == 10001:
+        return -1
+    return v[-1][-1]
+
