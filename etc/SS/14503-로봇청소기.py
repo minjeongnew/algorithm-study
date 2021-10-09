b624a064e47575dd39af3739135d78d05b296016
minjeongnew <20171623sw@kookmin.ac.kr>
2021-10-09 18:47:55 +0900

    gold5 구현&시뮬레이션/14503(로봇청소기)

---
+++ etc/SS/14503-로봇청소기.py
@@ -0,0 +1,48 @@
+# gold 5
+import sys
+from collections import deque
+from pprint import pprint
+
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+
+def change_direction(d):
+    return (d-1) % 4
+
+
+def back_direction(d):
+    return (d+2) % 4
+
+
+def solve(r, c, d):
+    q = deque()
+    q.append((r, c, d))
+    board[r][c] = 2
+    cnt = 1
+    while q:
+        x, y, direction = q.popleft()
+        for i in range(4):
+            direction = change_direction(direction)
+            nx = x + dx[direction]
+            ny = y + dy[direction]
+            if 0 <= nx < n and 0 <= ny < m and board[nx][ny] == 0:
+                board[nx][ny] = 2
+                cnt += 1
+                q.append((nx, ny, direction))
+                break
+            elif i == 3:
+                nx = x + dx[back_direction(direction)]
+                ny = y + dy[back_direction(direction)]
+                q.append((nx, ny, direction))
+                if board[nx][ny] == 1:
+                    return cnt
+
+
+if __name__ == "__main__":
+    n, m = map(int, sys.stdin.readline().split())
+    r, c, d = map(int, sys.stdin.readline().split()) # 로봇청소기 위치랑 방향
+    board = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+    print(solve(r, c, d))
+    # pprint(board)
