2da93e2b99bb19350677c02b7ef2d4ca1fd0ea9d
minjeongnew <20171623sw@kookmin.ac.kr>
2021-11-02 22:55:07 +0900

    silver5 구현&시뮬레이션/1913(달팽이)

---
+++ boj/silver/boj-silver5/1913-달팽이.py
@@ -0,0 +1,42 @@
+import sys
+
+n = int(sys.stdin.readline())
+p = int(sys.stdin.readline())
+
+board = [[0]*n for _ in range(n)]
+direction = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+mid = (n//2, n//2)
+board[mid[0]][mid[1]] = 1
+ax, ay = n//2, n//2
+
+def solve():
+    global ax, ay
+    rng = 0
+    num = 2
+    x = mid[0]
+    y = mid[1]
+    while True:
+        cnt = 0
+        for k in range(4):
+            if k == 0 or k == 2:
+                rng += 1
+            for _ in range(rng):
+                nx = x + direction[k][0]
+                ny = y + direction[k][1]
+                board[nx][ny] = num
+                x = nx
+                y = ny
+                if num == p:
+                    ax, ay = x, y
+                num += 1
+                cnt += 1
+                if num > n**2:
+                    return
+            if cnt == rng:
+                cnt = 0
+        if num >= n**2:
+            break
+solve()
+for i in range(n):
+    print(*board[i])
+print(ax+1, ay+1)
